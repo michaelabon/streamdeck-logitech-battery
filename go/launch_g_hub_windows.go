b4f21b342c8151cf2ef6a8518d370ed5977ec383
THOR-V2\offic <ddf>
2024-03-20 21:52:23 -0500

    Launch the correct application on Windows on keyUp

--- go/launch_g_hub_windows.go
+++ go/launch_g_hub_windows.go
@@ -4,23 +4,17 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"os/exec"
 	"path/filepath"
 )
 
 func launchGHub() error {
-	cmd := open("GHub.exe")
+	path := filepath.Join("C:\\", "Program Files", "LGHUB", "lghub.exe")
+	cmd := exec.Command(path)
 	err := cmd.Run()
 	if err != nil {
 		return fmt.Errorf("error while opening G Hub: %w", err)
 	}
 
 	return nil
-}
-
-func open(input string) *exec.Cmd {
-	runDll32 := filepath.Join(os.Getenv("SYSTEMROOT"), "System32", "rundll32.exe")
-
-	return exec.Command(runDll32, "url.dll,FileProtocolHandler", input)
 }
